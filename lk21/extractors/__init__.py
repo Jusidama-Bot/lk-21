7322f0f09520b11df3e75d3fe3b8d6c2b9a2af80
Ｊｕｓｉｄａｍａ Ｂｏｔ <97734754+Jusidama-Bot@users.noreply.github.com>
2022-07-20 21:47:49 +0700

    Update __init__.py

--- lk21/extractors/__init__.py
+++ lk21/extractors/__init__.py
@@ -51,8 +51,7 @@ class BaseExtractor:
             session = create_scraper() 
         else:
             session = requests.Session()
-        session.headers[
-            "User-Agent"] = "Mozilla/5.0 (Linux; Android 7.0; 5060 Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36"
+            session.headers["User-Agent"] = "Mozilla/5.0 (Linux; Android 7.0; 5060 Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/58.0.3029.83 Mobile Safari/537.36"
         session.cookies = LWPCookieJar()
         return session
 
